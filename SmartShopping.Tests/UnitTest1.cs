20800e6c9297c24a86f0ce073cac61366b7a249b
Luca <96955704+Json-exe@users.noreply.github.com>
2025-01-10 07:46:18 +0100

    feat(tests): add test case for remote api

---
+++ SmartShopping.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using SmartShopping.Lib;
+using SmartShopping.Lib.Api;
+
+namespace SmartShopping.Tests;
+
+public class UnitTest1
+{
+    private readonly ServiceProvider _services = new ServiceCollection()
+        .AddLib()
+        .BuildServiceProvider();
+    
+    [Fact]
+    public async Task TestOpenFoodFactsApiAndJsonDeserialization()
+    {
+        var client = _services.GetRequiredService<OpenFoodFactsApiClient>();
+        var response = await client.GetProductInformation("3017620422003");
+        response.Code.Should().Be("3017620422003");
+        response.Product.ProductName.Should().Contain("Nutella");
+        response.Product.Ingredients.Should().NotBeEmpty();
+        response.Status.Should().Be(1);
+    }
+}
